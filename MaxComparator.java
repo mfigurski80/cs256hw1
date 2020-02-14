93dd9dfd53acd54da954564cae325943557e7528
Mikolaj Figurski <meek.f80@gmail.com>
2020-02-14 12:06:58 -0500

    Implement MaxComparator

--- MaxComparator.java
+++ MaxComparator.java
@@ -1,6 +1,6 @@
 import java.util.Comparator;
 public class MaxComparator<K> implements Comparator<K> {
     public int compare(K a, K b) throws ClassCastException {
-        return 0;
+        return ((Comparable<K>)a).compareTo(b);
     }
 }
