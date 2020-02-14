4613b64b16382a52cb4feaa704ccd4ce5b1edfa6
Mikolaj Figurski <meek.f80@gmail.com>
2020-02-14 18:27:12 -0500

    Javadoc for BinaryHeap, MedianHeap

--- BinaryHeap.java
+++ BinaryHeap.java
@@ -10,20 +10,28 @@ public class BinaryHeap<K> extends Heap<K> {
         comp = comparator;
     }
 
+    /**
+     * Swaps nodes at positions i and j in the ArrayList
+     * @param i index of first item
+     * @param j index of second item
+     */
     private void swap(int i, int j){
         K temp = array.get(i);
-        // System.out.print("SWAP: ");
-        // System.out.print(i);
-        // System.out.print(", ");
-        // System.out.println(j);
-        // System.out.println(array.toString());
         array.set(i, array.get(j));
         array.set(j, temp);
-        // System.out.println(array.toString());
     }
+    /**
+     * Returns root node -- by default, the smallest value in the ArrayList
+     * @return item of type K
+     */
     public K root() {
         return array.get(0);
     }
+    /**
+     * Deletes root node and returns it -- by default, the smallest value in
+     * the ArrayList
+     * @return item of type K
+     */
     public K removeRoot() {
         K toReturn = root();
         swap(0, array.size() - 1);
@@ -31,21 +39,55 @@ public class BinaryHeap<K> extends Heap<K> {
         sink(0);
         return toReturn;
     }
+    /**
+     * Gets the index of the left child in the tree structure, given index of
+     * parent
+     * @param  i index of parent
+     * @return   index of left child
+     */
     private int left(int i) {
         return 2*i + 1;
     }
+    /**
+     * Gets the index of the right child in the tree structure, given index of
+     * parent
+     * @param  i index of parent
+     * @return   index of right child
+     */
     private int right(int i) {
         return 2*i + 2;
     }
+    /**
+     * Checks ArrayList to see if it contains the left child of given parent
+     * node in the tree structure.
+     * @param  i index of parent
+     * @return   whether or not a left child exists
+     */
     private boolean hasLeft(int i) {
         return left(i) < array.size();
     }
+    /**
+     * Checks ArrayList to see if it contains the right child of given parent
+     * node in the tree structure.
+     * @param  i index of parent
+     * @return   whether or not a right child exists
+     */
     private boolean hasRight(int i) {
         return right(i) < array.size();
     }
+    /**
+     * Gets the index of a given node's parent within the tree structure
+     * @param  i index of child
+     * @return   index of parent
+     */
     private int parent(int i) {
         return (i-1) / 2;
     }
+    /**
+     * Pushes the node at given index down the tree, until it gets to a
+     * position where its children have less priority.
+     * @param i index of node to push down
+     */
     private void sink(int i) {
         while (hasLeft(i)) {
             int leftIndex = left(i);
@@ -61,6 +103,11 @@ public class BinaryHeap<K> extends Heap<K> {
             i = smallChildIndex;
         }
     }
+    /**
+     * Pulls the node at the given index up the tree, until it gets to a
+     * position where its parent has more priority.
+     * @param i index of node to pull up
+     */
     private void swim(int i) {
         while (i > 0) {
             int p = parent(i);
@@ -72,26 +119,50 @@ public class BinaryHeap<K> extends Heap<K> {
             }
         }
     }
+    /**
+     * Adds a given node to the tree, and pulls it up to it's proper place
+     * @param k node to add
+     */
     public void insert(K k) {
         array.add(k);
-        // System.out.print("Inserting: ");
-        // System.out.println(k);
         swim(array.size() - 1);
     }
+    /**
+     * Deletes minimum node and returns it (ASSUMING THAT THE COMPARATOR IS THE
+     * DEFAULT COMPARATOR, AS OPPOSED TO MAX COMPARATOR)
+     * @return node of type K
+     */
     public K removeMin() {
         return removeRoot();
     }
+    /**
+     * Returns minimum node (ASSUMING THAT THE COMPARATOR IS THE DEFAULT
+     * COMPARATOR, AS OPPOSED TO MAX COMPARATOR)
+     * @return node of type K
+     */
     public K min() {
         return root();
     }
 
+    /**
+     * Creates a string representation of the Binary Heap's current state.
+     * @return String representation
+     */
     public String toString() {
         return array.toString();
     }
+    /**
+     * Checks the current amount of nodes in the heap
+     * @return Number of nodes
+     */
     public int size() {
         return array.size();
     }
 
+    /**
+     * Testing script...
+     * @param args Command line arguments. Will be sorted into BinaryHeap
+     */
     public static void main(String[] args) {
         BinaryHeap<String> a = new BinaryHeap<String>();
         for (String cur : args) {
--- MedianHeap.java
+++ MedianHeap.java
@@ -5,17 +5,26 @@ public class MedianHeap<K extends Comparable<K>> { // TODO: maybe not Comparable
     protected BinaryHeap<K> smallHeap = new BinaryHeap<K>(new MaxComparator<K>());
     protected BinaryHeap<K> largeHeap = new BinaryHeap<K>();
 
+    /**
+     * Checks if there is anything in either heap. Note, small heap is populated
+     * first, so if small heap is empty we know MedianHeap is empty as well.
+     * @return Whether this MedianHeap is empty
+     */
     public boolean isEmpty() {
         return (smallHeap.size() == 0);
     }
+    /**
+     * Finds the size of the current MedianHeap by adding both small and
+     * large heaps
+     * @return size of this MedianHeap
+     */
     public int size() {
         return smallHeap.size() + largeHeap.size();
     }
-    /*
-    private void rebalance(){
-        //perhaps you'd use a helper method like this...
-    }
-    */
+    /**
+     * Inserts an item into the MedianHeap, maintaining its structure
+     * @param k Item to insert
+     */
     public void insert(K k) {
         smallHeap.insert(k);
         while(smallHeap.size() > largeHeap.size() + 1) {
@@ -29,6 +38,10 @@ public class MedianHeap<K extends Comparable<K>> { // TODO: maybe not Comparable
             largeHeap.insert(smallHeap.removeRoot());
         }
     }
+    /**
+     * Removes the median item from the MedianHeap and returns it
+     * @return Item at current median
+     */
     public K removeMedian() {
         K to_return = smallHeap.removeRoot();
         while (smallHeap.size() < largeHeap.size()) {
@@ -36,14 +49,27 @@ public class MedianHeap<K extends Comparable<K>> { // TODO: maybe not Comparable
         }
         return to_return;
     }
+    /**
+     * Finds the current median item
+     * @return Item at current median
+     */
     public K median() {
         return smallHeap.root();
     }
 
+    /**
+     * Creates a String representation of this MedianHeap by combining String
+     * representations of auxiliary heaps
+     * @return String representation of this MedianHeap
+     */
     public String toString() {
         return smallHeap.toString() + " -- " + largeHeap.toString();
     }
 
+    /**
+     * Testing script...
+     * @param args Console arguments. These don't change program's execution
+     */
     public static void main(String[] args) {
         MedianHeap<Integer> a = new MedianHeap<Integer>();
         a.insert(1);
